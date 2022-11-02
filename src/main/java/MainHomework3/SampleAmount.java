eaed0c96d5f876724473f159815f537a590f050b
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-02 17:14:11 +0400

    2nd task done

---
+++ src/main/java/MainHomework3/SampleAmount.java
@@ -0,0 +1,15 @@
+package MainHomework3;
+
+public class SampleAmount {
+    public static void main(String[] args) {
+
+        int start = 1;
+        int k = 4;
+        int amount = (start);
+
+        do {
+            System.out.println("amount = " + amount);
+            amount = (start + (amount * k));
+        } while (amount <= 100);
+        }
+    }
---
+++ src/main/java/MainHomework3/SampleCycle.java
@@ -0,0 +1,9 @@
+package MainHomework3;
+
+public class SampleCycle {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
