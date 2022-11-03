3783a8594f0dbbc98ba2fd5ba7444143330bb2a3
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-03 18:37:03 +0400

    3rd task done

---
+++ src/main/java/MainHomework3/StrangeAmount.java
@@ -0,0 +1,25 @@
+package MainHomework3;
+
+public class StrangeAmount {
+    public static void main(String[] args) {
+
+        int oddAmount = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 1) {
+                oddAmount += i;
+            }
+        }
+        System.out.println("Сумма нечетных чисел = " + oddAmount);
+
+        int dividedAmount = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                dividedAmount += i;
+            }
+        }
+        System.out.println("Сумма четных чисел = " + dividedAmount);
+int total = (oddAmount - dividedAmount);
+        System.out.println("Сумма чисел в диапазоне [1 ; 100], если четные числа отрицательные, а нечетные -" +
+                " положительные = " + total);
+    }
+}
